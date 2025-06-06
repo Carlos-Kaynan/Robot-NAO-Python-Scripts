6a6cc56c69add04c0d3b8aacdee5f0a4298cf822
Carlos Kaynan <104600861+Carlos-Kaynan@users.noreply.github.com>
2025-06-06 08:36:31 -0300

    Update and rename Controle_NAO_Com_Cerebro to Controle_NAO_Com_Cerebro.py

--- Controle_NAO_Com_Cerebro.py
+++ Controle_NAO_Com_Cerebro.py
@@ -1,3 +1,13 @@
+'''
+Observações:
+Troque robot_ip = "192.168.1.100" pelo IP real do seu robô.
+
+O NAO precisa estar em uma superfície estável com espaço para andar.
+
+Certifique-se de que o OpenViBE esteja transmitindo dados via LSL corretamente.
+'''
+
+
 import time
 import threading
 from pylsl import StreamInlet, resolve_byprop
