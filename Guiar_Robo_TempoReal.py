d4e6c18b5b94df3c6135a20ee691226c2b05b713
Carlos Kaynan <104600861+Carlos-Kaynan@users.noreply.github.com>
2026-04-23 10:16:02 -0300

    Update Guiar_Robo_TempoReal.py

--- Guiar_Robo_TempoReal.py
+++ Guiar_Robo_TempoReal.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #Instale as bibliotecas pynput, pynaoqi e naoqi
-#Use Python 2.7 apenas only só apenas 
+#Use Python 2.7 apenas only só apenas somente
 #baixe o SDK
 
 """
